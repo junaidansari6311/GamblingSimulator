c7ec5bcb751529ab1a8243e3330be8cc29c7a431
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-27 11:35:48 +0530

    ADDED:Checking winning or loosing bet

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,5 +1,13 @@
+#!/bin/bash -x
 echo "WELCOME TO GAMBLING SIMULATOR" 
 #constants
 BET=1
 #variables
 stake=100
+
+if [ $((RANDOM%2)) -eq 1 ]
+then
+	stake=$(($stake + $BET))
+else
+	stake=$(($stake - $BET))
+fi
