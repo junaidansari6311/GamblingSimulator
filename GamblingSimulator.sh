c80855942cdfbf372239600b3790fdf391a3e64b
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-27 12:54:18 +0530

    ADDED:calulations for winning and loosing in 20days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,19 +6,35 @@ WINNING_LIMIT=150
 LOOSING_LIMIT=50
 #variables
 stake=100
-while [[ $stake -lt WINNING_LIMIT && $stake -gt LOOSING_LIMIT ]]
+winningAmount=0
+loosingAmount=0
+for ((i=1;i<=20;i++))
 do
-	if [ $((RANDOM%2)) -eq 1 ]
+	while [[ $stake -lt $WINNING_LIMIT && $stake -gt $LOOSING_LIMIT ]]
+	do
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			stake=$(($stake + $BET))
+		else
+			stake=$(($stake - $BET))
+		fi
+	done
+
+	if [ $stake -ge $WINNING_LIMIT ]
 	then
-		stake=$(($stake + $BET))
+		winningAmount=$(($winningAmount+50))
+		stake=100
 	else
-		stake=$(($stake - $BET))
+		loosingAmount=$(($loosingAmount+50))
+		stake=100
 	fi
 done
-
-if [ $stake -ge $WINNING_LIMIT ]
+if [ $winningAmount -gt $loosingAmount ]
 then
-	echo "Gambler won and resigned for the day"
+	echo "You won by $(($winningAmount - $loosingAmount)) in 20 days" 
+elif [ $loosingAmount -gt $winningAmount ]
+then
+	echo "You lost by $(($loosingAmount - $winningAmount)) in 20 days"
 else
-	echo "Gambler lost and resigned for the day"
+	echo "You neither won nor lost "
 fi
