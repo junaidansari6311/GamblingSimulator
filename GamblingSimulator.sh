4bb696b986b1669008bbb1669218aaa9518aa613
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-28 18:22:20 +0530

    ADDED: function for checking luckiest and unluckiest day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,5 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "WELCOME TO GAMBLING SIMULATOR" 
+#dictionary
+declare -A wonOrLostPerDay
 #constants
 BET=1
 WINNING_LIMIT=150
@@ -9,9 +11,11 @@ NO_OF_DAYS=20
 stake=100
 winningAmount=0
 loosingAmount=0
-WonCount=0
-LostCount=0
-for ((i=1;i<=$NO_OF_DAYS;i++))
+wonCount=0
+lostCount=0
+presentAmount=0
+
+for ((i=1;i<=$NO_OF_DAYS; i++))
 do
 	while [[ $stake -lt $WINNING_LIMIT && $stake -gt $LOOSING_LIMIT ]]
 	do
@@ -26,13 +30,16 @@ do
 	if [ $stake -ge $WINNING_LIMIT ]
 	then
 		winningAmount=$(($winningAmount+50))
+		presentAmount=$(($presentAmount + 50))
 		stake=100
 		((wonCount++))
 	else
 		loosingAmount=$(($loosingAmount+50))
+		presentAmount=$(($presentAmount - 50))
 		stake=100
 		((lostCount++))
 	fi
+	wonOrLostPerDay[$i]=$presentAmount
 done
 if [ $winningAmount -gt $loosingAmount ]
 then
@@ -48,3 +55,19 @@ echo "No of days won : $wonCount"
 echo "No of days lost  : $lostCount"
 echo "Amount won : $(($wonCount*50))"
 echo "Amount lost : $(($lostCount*50))"
+
+echo ${!wonOrLostPerDay[@]}
+echo ${wonOrLostPerDay[@]}
+
+function checkLuckiestAndUnlucliestDay () {
+	for i in ${!wonOrLostPerDay[@]}
+	do
+		echo "$i ${wonOrLostPerDay[$i]}"
+	done | sort -k2 $1 | head -1
+}
+
+echo "Luckiest day"
+checkLuckiestAndUnlucliestDay -rn
+echo "Unluckiest day"
+checkLuckiestAndUnlucliestDay -n
+
